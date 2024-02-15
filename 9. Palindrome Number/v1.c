65338020c95a7ab5ba571f663929b1edb82e4bf4
chuan0306 <62919709+chuan0306@users.noreply.github.com>
2024-02-15 17:51:33 +0800

    Create v1.c

---
+++ 9. Palindrome Number/v1.c
@@ -0,0 +1,33 @@
+bool isPalindrome(int x) {
+
+    if(x<0){
+        return false;
+    }else{
+        int length = 0;
+        int tmp = x;
+
+        while(tmp > 0){
+            int a = tmp % 10;
+            tmp = (tmp - a) / 10;
+            length += 1;
+        }
+
+        int round = length / 2;
+        int input[length];
+        tmp = x;
+
+        // int i = length - 1; i>=0; i--
+        for(int i=0; i<length; i++){
+            int a = tmp % 10;
+            input[i] = a;
+            tmp = (tmp - a) / 10;
+        }
+
+        for(int i=0; i<round; i++){
+            if(input[i] != input[length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
